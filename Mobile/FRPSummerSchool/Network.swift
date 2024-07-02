ee62cfee85f30bb22962d0dceac6dc8843e83d84
ilia-ch <ilia-ch@yandex-team.ru>
2024-07-02 15:26:47 +0300

    Замечания

--- Mobile/FRPSummerSchool/Network.swift
+++ Mobile/FRPSummerSchool/Network.swift
@@ -44,6 +44,19 @@ final class Network {
 
     private func load<T: Decodable>(request: URLRequest) -> Single<T> {
         Single<T>.create { single in
+            // TODO: - показать отмену подписки
+            // TODO: - четко давать формулировки и определения
+            // TODO: - расскрыть тему сабжектов
+            // TODO: - сначала презентация, потом проект
+            // TODO: - рассказать про .rx в RxCocoa
+            // про шедулеры неправильно рассказал
+            // Подписывать в subscride/bind - очень опасно - исправить бы это
+            // flatMap - событие одного стрима порождает другой стрим. Раскрыть тему подробнее
+            // показать на тестах работу debounce
+            // баги показать на суперпростых примерах
+            // subscribe(on:), observer(on:) раскрыть подробнее, вызывать в конце цепочек, бест практис
+            // добавить пример с авторизацией
+
             URLSession.shared.dataTask(with: request) { data, response, error in
                 guard error == nil else {
                     single(.failure(error!))
