63c4707ff54e1edb39509c44958474a3d25b2206
ilia-ch <ilia-ch@yandex-team.ru>
2024-07-12 10:54:04 +0300

    Добавлено добавление в корзину (с багом)

--- Mobile/FRPSummerSchool/ViewController.swift
+++ Mobile/FRPSummerSchool/ViewController.swift
@@ -54,10 +54,17 @@ class ViewController: UIViewController {
         storage.items
             .bind(
                 to: collectionView.rx.items(cellIdentifier: ItemCell.id, cellType: ItemCell.self)
-            ) { index, item, cell in
+            ) { [unowned self] index, item, cell in
                 cell.item = item
+                cell.plusTap
+                    .drive(onNext: storage.addItem(id:))
+                    .disposed(by: disposeBag)
+                cell.minusTap
+                    .drive(onNext: storage.removeItem(id:))
+                    .disposed(by: disposeBag)
             }
             .disposed(by: disposeBag)
+        
     }
 
     // MARK: - Private methods
